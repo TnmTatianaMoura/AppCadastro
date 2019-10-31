d74814a733c0d6429745a9f8a39a90acbb165526
Tatiana <tatycat3@gmail.com>
2019-10-31 18:16:03 -0300

    segundo commit

--- app/src/main/java/projetomobile/cadastro/com/activity/AnunciosActivity.java
+++ app/src/main/java/projetomobile/cadastro/com/activity/AnunciosActivity.java
@@ -1,5 +1,6 @@
 package projetomobile.cadastro.com.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import projetomobile.cadastro.com.R;
 import projetomobile.cadastro.com.TelaLogin.LoginActivity;
@@ -17,52 +18,53 @@ public class AnunciosActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
 
-        //Configurações iniciais
+        //Configurações Iniciais
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
-        autenticacao.signOut();
+        //autenticacao.signOut();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
 
-        if( autenticacao.getCurrentUser() == null ){//usuario deslogado
-            menu.setGroupVisible(R.id.group_deslogado,true);
-        }else {//Usuario logado
-            menu.setGroupVisible(R.id.group_logado, true);
-        }
+        if(autenticacao.getCurrentUser() == null){//usuario deslogado
 
+            menu.setGroupVisible(R.id.group_deslogado,true);
+        }else {//usuario logado
+
+            menu.setGroupVisible(R.id.group_logado,true);
+        }
         return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        switch ( item.getItemId() ){
+        switch (item.getItemId() ){
             case R.id.menu_cadastrar :
-                startActivity( new Intent(getApplicationContext(), LoginActivity.class));
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 break;
             case R.id.menu_sair :
                 autenticacao.signOut();
                 invalidateOptionsMenu();
                 break;
             case R.id.menu_anuncios :
-
                 startActivity(new Intent(getApplicationContext(), MeusAnunciosActivity.class));
-            break;
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
